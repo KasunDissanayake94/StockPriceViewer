218e0e76f70da200939a093b6e998c831a3d9ad5
Kasun Dissanayake <kasund@hsenidlanka.com>
2020-05-19 15:47:49 +0530

    quote service added

---
+++ quote-service/src/test/java/com/kasundissanayake/quote/quoteservice/QuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasundissanayake.quote.quoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
